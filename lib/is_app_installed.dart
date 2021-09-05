069fd2229f90c1befcf266117dee997a7a24db4a
Your Name <you@example.com>
2021-09-05 11:44:22 +0800

    add null safety

--- lib/is_app_installed.dart
+++ lib/is_app_installed.dart
@@ -5,18 +5,18 @@ import 'package:flutter/services.dart';
 class IsAppInstalled {
   static const MethodChannel _channel = const MethodChannel('is_app_installed');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
+  static Future<String?> get platformVersion async {
+    final String? version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
 
-  static Future<bool> isAppInstalled(String packageNameOrScheme) async {
-    final bool installed = await _channel.invokeMethod('isAppInstalled', packageNameOrScheme);
+  static Future<bool?> isAppInstalled(String packageNameOrScheme) async {
+    final bool? installed = await _channel.invokeMethod('isAppInstalled', packageNameOrScheme);
     return installed;
   }
 
-  static Future<bool> jumpTo(String schemeUrl) async {
-    final bool installed = await _channel.invokeMethod('jumToUrl', schemeUrl);
+  static Future<bool?> jumpTo(String schemeUrl) async {
+    final bool? installed = await _channel.invokeMethod('jumToUrl', schemeUrl);
     return installed;
   }
 }
